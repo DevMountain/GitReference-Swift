8d5a0af4b68d97439699263097d43a13f3587642
Joshua Howland <development@jkhowland.com>
2014-08-10 14:55:23 -0600

    Initial Commit

---
+++ GitReference-Swift/AppDelegate.swift
@@ -0,0 +1,28 @@
+//
+//  AppDelegate.swift
+//  GitReference-Swift
+//
+//  Created by Joshua Howland on 8/10/14.
+//  Copyright (c) 2014 DevMountain. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    var window: UIWindow?
+    var navigationController: UINavigationController?
+    
+    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: NSDictionary?) -> Bool {
+        
+        self.window = UIWindow(frame: UIScreen.mainScreen().bounds)
+        // Override point for customization after application launch.
+        self.window!.backgroundColor = UIColor.whiteColor()
+        self.window!.makeKeyAndVisible()
+        
+        return true
+        
+    }
+    
+}
